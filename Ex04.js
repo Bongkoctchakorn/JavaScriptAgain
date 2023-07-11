42b92c6654069592f2da4b121995a95cc3d9b8a0
Bongkoctchakorn <bkkam001@gnail.com>
2023-07-11 18:55:41 +0700

    cry

---
+++ Ex04.js
@@ -0,0 +1,10 @@
+function dividesEvanly(num1,num2){
+    if (num1%num2 == 0) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(dividesEvanly(98,7));
+console.log(dividesEvanly(85,4));
